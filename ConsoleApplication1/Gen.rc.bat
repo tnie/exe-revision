05fd2b8d1b0e6d3d290afa516c6d7d6c3654fe0d
nielyw <nielyw@9a7c71a5-0b07-45d0-bdd4-76dc3152a42c>
2020-02-26 07:07:30 +0000

    Reduce to one template: move version.rc.svn version.svn.rc; remove version.rc.git
    git-svn-id: https://svn.code.sf.net/p/exe-revision/svn/trunk@17 9a7c71a5-0b07-45d0-bdd4-76dc3152a42c

--- ConsoleApplication1/Gen.rc.bat
+++ ConsoleApplication1/Gen.rc.bat
@@ -1,10 +1,17 @@
 @echo off
 
+:: replace $TARGETFILENAME$ with %1
+echo TargetFileName is "%1"
+powershell -Command "(Get-Content version.svn.rc) -replace '\$TARGETFILENAME\$', '%1' | Out-File  version.rc"
+
 :SVN
 :: https://tortoisesvn.net/docs/release/TortoiseSVN_zh_CN/tsvn-subwcrev.html
 echo Maybe Subversion WorkingCopyPath ...
-subwcrev.exe ../ version.rc.svn version.rc -nNm
-if %errorlevel% == 10 goto GIT
+subwcrev.exe ../ version.rc version.rc -nNm
+if %errorlevel% == 10 (
+	powershell -Command "(Get-Content version.rc) -replace '\$WCREV\$', '$WCREV=7$' | Out-File  version.rc"
+	goto GIT
+)
 goto END
 
 :DUMMY
@@ -13,7 +20,7 @@ ehco Do nothing.
 :GIT
 :: https://tortoisegit.org/docs/tortoisegit/tgit-gitwcrev.html
 echo Not SVN, try GIT ...
-gitwcrev.exe ../ version.rc.git version.rc -mu
+gitwcrev.exe ../ version.rc version.rc -mu
 
 :END
 echo Exit with %errorlevel%
